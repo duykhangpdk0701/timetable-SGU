3f68f3e42f5d90bc8675a89b4dc36143b80e9755
khang phung <duykhangpdk0701@gmail.com>
2022-12-22 01:09:43 +0700

    little change

--- client/src/constants/backgroudColorGroup.js
+++
@@ -1,8 +0,0 @@
-export const backgroundColor = [
-  { background: "#DEF7F8", border: "#8CE0E1" },
-  { background: "#FEF8E8", border: "#FFE5A9" },
-  { background: "#F7E8FD", border: "#E7BBFC" },
-  { background: "#F7F7F7", border: "#DEDEDE" },
-  { background: "#FFE8E4", border: "#F6846F" },
-  { background: "#FDEFE4", border: "#F2AE68" },
-];
--- native/src/global/color.js
+++
@@ -1,3 +0,0 @@
-export const grayColor = "#f0f0f0";
-
-export const grayTextColor = "#a3a3a3";
