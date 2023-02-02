727aa01ac7c3f3daf6d6705030824ff7a4150982
khang phung <duykhangpdk0701@gmail.com>
2023-02-02 14:41:39 +0700

    add folder api

---
+++ server/api/index.js
@@ -0,0 +1,15 @@
+const Route = require("../src/routers/index");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+Route(app);
+
+app.listen(process.env.PORT || 4000, () => {
+  console.log("Server is running on " + process.env.PORT + "...");
+});
